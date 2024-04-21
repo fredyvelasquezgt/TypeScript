57094e21a33b6c006ee5f517e239182c5b6785ae
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-20 21:08:24 -0600

    ts

--- bases/app.js
+++ bases/app.js
@@ -1,3 +1,7 @@
 "use strict";
 const a = 10;
-let b = 10;
+let b;
+//this is a bad practice cause other devs won't know what is the data type that this function receives
+function sayHello(msg) {
+    console.log(msg);
+}
--- bases/app.ts
+++ bases/app.ts
@@ -1,2 +1,7 @@
 const a:number = 10;
-let b:number = 10;
+let b:string;
+
+//this is a bad practice cause other devs won't know what is the data type that this function receives
+function sayHello(msg) {
+    console.log(msg)
+}
