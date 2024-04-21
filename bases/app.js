f9d95cd58830f900ed3148803f6b3f3078172629
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-20 23:39:17 -0600

    ts

--- bases/app.js
+++ bases/app.js
@@ -1,7 +1,12 @@
 "use strict";
-const a = 10;
-let b;
+//anonymous function auto invoked
+(() => {
+    const a = 10;
+    //let b:string;
+    console.log(a);
+})();
+//let b:string;
 //this is a bad practice cause other devs won't know what is the data type that this function receives
-function sayHello(msg) {
-    console.log(msg);
-}
+// function sayHello(msg:string) {
+//     console.log(msg + ''+'Fredy')
+// }
--- bases/app.ts
+++ bases/app.ts
@@ -1,7 +1,14 @@
+//anonymous function auto invoked
+(() => {
 const a:number = 10;
-let b:string;
+//let b:string;
+
+console.log(a)
+})()
+
+//let b:string;
 
 //this is a bad practice cause other devs won't know what is the data type that this function receives
-function sayHello(msg) {
-    console.log(msg)
-}
+// function sayHello(msg:string) {
+//     console.log(msg + ''+'Fredy')
+// }
