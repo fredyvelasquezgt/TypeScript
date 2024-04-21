51b37310cce2294f25510e3c51bc8a3e1e07c77b
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-20 23:53:10 -0600

    ts

---
+++ bases/tipos/strings.js
@@ -0,0 +1,5 @@
+"use strict";
+(() => {
+    const batman = 'batman';
+    const greenLantern = "Green's Lantern";
+})();
---
+++ bases/tipos/strings.ts
@@ -0,0 +1,7 @@
+(() => {
+
+    const batman = 'batman';
+    const greenLantern = "Green's Lantern"
+
+
+})()
