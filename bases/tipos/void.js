54676708e347cd25f2fe52585a12f605a4854e0b
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-21 23:34:40 -0600

    ts

---
+++ bases/tipos/void.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    function callBatman() {
+        return 1;
+    }
+    const a = callBatman();
+    console.log(a);
+})();
---
+++ bases/tipos/void.ts
@@ -0,0 +1,11 @@
+(() => {
+
+    function callBatman():void {
+        return 1;
+    }
+
+    const a = callBatman()
+    console.log(a)
+
+
+})()
