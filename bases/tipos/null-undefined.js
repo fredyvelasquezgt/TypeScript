f2033f82f8973cf6d3df947166bd5052cba4986d
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-22 00:02:34 -0600

    ts

--- bases/tipos/null-undefined.js
+++ bases/tipos/null-undefined.js
@@ -1,5 +1,8 @@
 "use strict";
 (() => {
-    let nada = undefined;
-    console.log(nada);
+    //this is great when strictNullChecks is TRUE
+    let isActive = null;
+    // let nada: number = undefined;
+    // console.log(nada)
+    console.log(isActive);
 })();
--- bases/tipos/null-undefined.ts
+++ bases/tipos/null-undefined.ts
@@ -1,6 +1,9 @@
 (() => {
 
-    let nada: number = undefined;
-    console.log(nada)
+    //this is great when strictNullChecks is TRUE
+    let isActive:boolean = null;
+    // let nada: number = undefined;
+    // console.log(nada)
+    console.log(isActive)
 
 })()
