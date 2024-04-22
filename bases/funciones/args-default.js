b3b7025b49e810eaf0bb77405c34366ebf57fb31
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-22 16:04:06 -0600

    ts

---
+++ bases/funciones/args-default.js
@@ -0,0 +1,15 @@
+"use strict";
+//default parameters
+(() => {
+    //the optionals arguments should be at the end
+    const fullName = (firstName, lastName, upper = false) => {
+        if (upper) {
+            return `${firstName} ${lastName || 'no last name'}`.toUpperCase();
+        }
+        else {
+            return `${firstName} ${lastName || 'no last name'}`;
+        }
+    };
+    const name = fullName('Tony', 'Stark', true);
+    console.log({ name });
+})();
---
+++ bases/funciones/args-default.ts
@@ -0,0 +1,18 @@
+//default parameters
+(() => {
+
+    //the optionals arguments should be at the end
+    const fullName = (firstName:string, lastName?:string, upper: boolean = false):string => {
+
+        if(upper) {
+            return `${firstName} ${lastName || 'no last name'}`.toUpperCase()
+        } else {
+            return `${firstName} ${lastName || 'no last name'}`
+
+        }
+    }
+
+    const name = fullName('Tony', 'Stark', true)
+    console.log({name})
+
+})()
