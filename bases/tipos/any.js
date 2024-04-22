745f398e1b3ddef7780c23592dc7d55cb5b465ca
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-21 23:00:09 -0600

    casting

--- bases/tipos/any.js
+++ bases/tipos/any.js
@@ -4,4 +4,7 @@
     let exists;
     let power;
     avenger = 'doctor strange';
+    console.log(avenger.charAt(0));
+    avenger = 150.2;
+    console.log(avenger.toFixed(2));
 })();
--- bases/tipos/any.ts
+++ bases/tipos/any.ts
@@ -4,6 +4,10 @@
     let exists;
     let power;
 
-    avenger = 'doctor strange'
+    avenger = 'doctor strange';
+    console.log((avenger as string).charAt(0));
+
+    avenger = 150.2;
+    console.log((<number>avenger).toFixed(2))
 
 })()
