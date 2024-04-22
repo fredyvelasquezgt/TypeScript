9db97d993e09eeba29f65741bbc4ccfa3641610d
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-22 15:49:10 -0600

    ts

--- bases/funciones/functions.js
+++ bases/funciones/functions.js
@@ -1,1 +1,11 @@
 "use strict";
+(() => {
+    const hero = 'flash';
+    function returnName() {
+        return hero;
+    }
+    const activateSignal = () => {
+        return 'batisignal activate';
+    };
+    console.log(typeOf());
+})();
---
+++ bases/funciones/functions.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    const hero:string = 'flash'
+
+    function returnName():string {
+        return hero;
+    }
+
+    const activateSignal = ():string =>  {
+        return 'batisignal activate';
+    }
+
+    console.log(typeOf())
+})()
