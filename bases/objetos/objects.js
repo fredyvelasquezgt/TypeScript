256be9c22139da557adc33a2ae0e7fd52818518c
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-25 11:47:49 -0600

    ts

---
+++ bases/objetos/objects.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super Velocidad', 'Time Travel']
+    };
+    flash = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super Fuerza'],
+        getNombre() {
+            return this.name;
+        }
+    };
+})();
