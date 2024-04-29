9ecaa82b421ae7bcaf2c88c114c2061093a20d87
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-28 23:18:19 -0600

    ts

--- ES6/classes-es6.js
+++ ES6/classes-es6.js
@@ -1,14 +1,22 @@
 (() => {
     class Avenger {
-        name;
-        power;
-
-        constructor(name, power) {
+       
+        constructor(name='No name', power=123) {
             this.name = name;
             this.power = power;
         }
     }
 
-    const hulk = new Avenger()
+    class FlyingAvenger extends Avenger {
+        //execute this code when the class is called
+        constructor(name, power) {
+            super(name, power);
+            this.flying = true;
+        }
+    }
+
+    const hulk = new Avenger('Hulk', 9001)
+    const falcon = new FlyingAvenger('Falcon', 500)
     console.log(hulk)
+    console.log(falcon)
 })()
