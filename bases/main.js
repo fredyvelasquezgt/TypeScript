f70ebef7aec6dd0623c2660fe6272624c8bdea95
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-28 23:28:17 -0600

    ts

--- bases/main.js
+++ bases/main.js
@@ -1,6 +1,15 @@
 "use strict";
 (() => {
     class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
     }
+    Avenger.avgAge = 35; //can access them by reference to the class, not the instance
+    const antman = new Avenger('Antman', 'teamCap');
+    console.log(antman);
+    console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
--- classes/basica.ts
+++ classes/basica.ts
@@ -2,6 +2,19 @@
     class Avenger {
         private name: string; //only acces within this class
         public team:string; //acces outside of the class
-        public realName: string;
+        public realName?: string;
+        static avgAge: number = 35; //can access them by reference to the class, not the instance
+
+        constructor(name: string, team: string, realName?: string) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
     }
+
+    const antman: Avenger = new Avenger('Antman', 'teamCap');
+    console.log(antman)
+
+    console.log(Avenger.avgAge)
+
 })()
