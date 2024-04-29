643b5bd101bfe971af31fc80baf10b62c736087f
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-28 23:37:20 -0600

    ts

--- bases/main.js
+++ bases/main.js
@@ -1,14 +1,21 @@
 "use strict";
 (() => {
     class Avenger {
+        //define constructor and props
         constructor(name, team, realName) {
+            // this.name = name;
+            // this.team = team;
+            // this.realName = realName;
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
     }
+    //private name: string; //only acces within this class
+    //public team:string; //acces outside of the class
+    //public realName?: string;
     Avenger.avgAge = 35; //can access them by reference to the class, not the instance
-    const antman = new Avenger('Antman', 'teamCap');
+    const antman = new Avenger('Antman', 'teamCap', 'Scott Lang');
     console.log(antman);
     console.log(Avenger.avgAge);
 })();
--- classes/basica.ts
+++ classes/basica.ts
@@ -1,18 +1,27 @@
 (() => {
     class Avenger {
-        private name: string; //only acces within this class
-        public team:string; //acces outside of the class
-        public realName?: string;
+        //private name: string; //only acces within this class
+        //public team:string; //acces outside of the class
+        //public realName?: string;
         static avgAge: number = 35; //can access them by reference to the class, not the instance
 
-        constructor(name: string, team: string, realName?: string) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
+        //define constructor and props
+        constructor(
+            private name: string, 
+            private team: string, 
+            public realName?: string,
+          ) {
+            // this.name = name;
+            // this.team = team;
+            // this.realName = realName;
+
         }
+
+        bio() 
+
     }
 
-    const antman: Avenger = new Avenger('Antman', 'teamCap');
+    const antman: Avenger = new Avenger('Antman', 'teamCap', 'Scott Lang');
     console.log(antman)
 
     console.log(Avenger.avgAge)
