83c3dc2462270a443ee93d82e7af468b693b1233
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-28 21:54:47 -0600

    ts

--- ES6/desestructuracion.ts
+++ ES6/desestructuracion.ts
@@ -1,9 +1,26 @@
 (() => {
-    const avenger = {
+
+    type Avengers = {
+        nick: string;
+        ironman: string;
+        vision: string;
+        activo: boolean;
+        poder: number;
+    }
+
+    const avengers: Avengers = {
         nick: 'Samuel L Jackson',
         ironman: 'Robert Downey Jr',
         vision: 'Paul Bettany',
         activo: true,
         poder: 1500
     }
+
+    // const {poder, vision} = avenger
+    // console.log(poder.toFixed(), vision.toUpperCase())
+
+    const printAvenger = ({vision, ...resto}:Avengers) => {
+        console.log(vision, resto)
+    }
+
 })()
