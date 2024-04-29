2ec1b41b53720de047196b6357f9f925287074e6
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-28 21:48:10 -0600

    ts

---
+++ ES6/desestructuracion.ts
@@ -0,0 +1,9 @@
+(() => {
+    const avenger = {
+        nick: 'Samuel L Jackson',
+        ironman: 'Robert Downey Jr',
+        vision: 'Paul Bettany',
+        activo: true,
+        poder: 1500
+    }
+})()
