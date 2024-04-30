4fa6b02e7ef071025914f9ca0b278be68a8dbc97
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-30 16:13:49 -0600

    interface

--- inferfaces/complejas.ts
+++ inferfaces/complejas.ts
@@ -1,10 +1,36 @@
 (() => {
 
-    
+    interface Client {
+        name: string,
+        age?: number,
+        address?: Address
+    }
 
-    const client = {
+    interface Address {
+        id: number,
+        zip: string,
+        city:string
+    }
+
+    const client: Client = {
         name: 'Fredy',
-        age: 25
+        age: 25,
+        address: {
+            id: 125,
+            zip: 'ABC',
+            city: 'Ottawa'
+
+        }
+    }
+
+    const client2: Client = {
+        name: "Melissa", 
+        age: 30,
+        address: {
+            city: 'Toronto',
+            id: 120,
+            zip: 'ABC'
+        }
     }
 
 
