82389c25cbe63fec64e5503ee189c1986b03c25d
Fredy Velásquez <macacoloco696@gmail.com>
2024-05-03 10:50:51 -0600

    ts

--- bases/app.ts
+++ bases/app.ts
@@ -1,167 +1,51 @@
-type Carro = {
-    carroceria: string,
-    modelo:string,
-    antibalas: boolean,
-    pasajeros: number,
-    disparar?: () => void; //optional function
+// Crear interfaces
+
+// Cree una interfaz para validar el auto (el valor enviado por parametro)
+const conducirBatimovil = ( auto ):void => {
+  auto.encender = true;
+  auto.velocidadMaxima = 100;
+  auto.acelear();
 }
 
-// Objetos
-const batimovil:Carro = {
-  carroceria: "Negra",
-  modelo: "6x6",
-  antibalas: true,
-  pasajeros:4
-};
-
-const bumblebee: Carro = {
-  carroceria: "Amarillo con negro",
-  modelo: "4x2",
-  antibalas: true,
-  pasajeros:4,
-  disparar(){ // El metodo disparar es opcional
-    console.log("Disparando");
+const batimovil = {
+  encender:false,
+  velocidadMaxima:0,
+  acelear(){
+    console.log("...... gogogo!!!");
   }
-};
-
-
-// Villanos debe de ser un arreglo de objetos personalizados
-type Villian = {
-    nombre: string,
-    edad: number | undefined,
-    mutante: boolean
-}
-const villanos: Villian[] = [{
-  nombre:"Lex Luthor",
-  edad: 54,
-  mutante:false
-},{
-  nombre: "Erik Magnus Lehnsherr",
-  edad: 49,
-  mutante: true
-},{
-  nombre: "James Logan",
-  edad: undefined,
-  mutante: true
-}];
-
-// Multiples tipos
-// cree dos tipos, uno para charles y otro para apocalipsis
-
-type Charles = {
-    poder: string,
-    estatura: number
 }
 
-const charles: Charles = {
-  poder:"psiquico",
-  estatura: 1.78
-};
+// Cree una interfaz con que permita utilzar el siguiente objeto
+// utilizando propiedades opcionales
 
+const guason = {
+  reir: true,
+  comer:true,
+  llorar:false
+}
 
-type Apocalipsis = {
-    lider: boolean,
-    miembros: string[]
+const reir = ( guason ):void => {
+  if( guason.reir ){
+    console.log("JAJAJAJA");
+  }
 }
 
 
-const apocalipsis: Apocalipsis = {
-  lider:true,
-  miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+// Cree una interfaz para la siguiente funcion
+
+const ciudadGotica = ( ciudadanos:string[] ):number => {
+  return ciudadanos.length;
 }
 
-console.log(apocalipsis)
-// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
-let mystique: (Charles | Apocalipsis);
+// Cree una interfaz que obligue crear una clase
+// con las siguientes propiedades y metodos
 
-mystique = charles;
-mystique = apocalipsis;
-
-
-//TAREA 2
-// Funciones Básicas
-// function sumar( a: number, b:number ): number{
-//   return a + b;
-// }
-
-// const contar = ( heroes:string[] ):number => {
-//   return heroes.length;
-// }
-// const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-// contar(superHeroes);
-
-// //Parametros por defecto
-// const llamarBatman = ( llamar:boolean = true ): void => {
-//   if( llamar ){
-//     console.log("Batiseñal activada");
-//   }
-// }
-
-// llamarBatman();
-
-// // Rest?
-// const unirheroes = ( ...personas:string[]): string => {
-//   return personas.join(", ");
-// }
-
-// // Tipo funcion
-// const noHaceNada = ( numero:number, texto:string, booleano:boolean, arreglo: string[] ) => {}
-
-// // Crear el tipo de funcion que acepte la funcion "noHaceNada"
-// let noHaceNadaTampoco: (n:number, t:string, b:boolean, a:string[]) => void;
-// noHaceNadaTampoco = noHaceNada
-
-
-
-
-
-
-
-// TAREA #1
-
-// TIPADO NORMAL
-
-//(() => {
-
-//   // Tipos
-//   const batman:string = 'Bruce';
-//   const superman:string = 'Clark';
-
-//   const existe:boolean = false;
-
-//   // Tuplas
-//   const parejaHeroes: [string, string] = [batman,superman];
-//   const villano: [string, number, boolean] = ['Lex Lutor',5,true];
-
-//   // Arreglos
-//   const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
-
-//   //Enumeraciones
-//   enum PoderesHeroes {
-//     acuaman = 0,
-//     batman = 1,
-//     flash = 5,
-//     superman = 100
-//   }
-
-//     const fuerzaAcuaman: PoderesHeroes = PoderesHeroes.acuaman;
-//     const fuerzaBatman: PoderesHeroes = PoderesHeroes.batman;
-//     const fuerzaFlash: PoderesHeroes = PoderesHeroes.flash;
-//     const fuerzaSuperman: PoderesHeroes = PoderesHeroes.superman;
-
-//   // Retorno de funciones
-//   function activar_batiseñal():string{
-//     return 'activada';
-//   }
-
-//   function pedir_ayuda():void{
-//     console.log('Auxilio!!!');
-//   }
-
-//   // Aserciones de Tipo
-//   const poder: any = '100';
-//   const largoDelPoder:number = (poder as string).length;
-//   console.log( largoDelPoder );
-
-
-// })()
+/*
+  propiedades:
+    - nombre
+    - edad
+    - sexo
+    - estadoCivil
+    - imprimirBio(): void // en consola una breve descripcion.
+*/
+class Persona {}
---
+++ homework/app.ts
@@ -0,0 +1,167 @@
+// type Carro = {
+//     carroceria: string,
+//     modelo:string,
+//     antibalas: boolean,
+//     pasajeros: number,
+//     disparar?: () => void; //optional function
+// }
+
+// // Objetos
+// const batimovil:Carro = {
+//   carroceria: "Negra",
+//   modelo: "6x6",
+//   antibalas: true,
+//   pasajeros:4
+// };
+
+// const bumblebee: Carro = {
+//   carroceria: "Amarillo con negro",
+//   modelo: "4x2",
+//   antibalas: true,
+//   pasajeros:4,
+//   disparar(){ // El metodo disparar es opcional
+//     console.log("Disparando");
+//   }
+// };
+
+
+// // Villanos debe de ser un arreglo de objetos personalizados
+// type Villian = {
+//     nombre: string,
+//     edad: number | undefined,
+//     mutante: boolean
+// }
+// const villanos: Villian[] = [{
+//   nombre:"Lex Luthor",
+//   edad: 54,
+//   mutante:false
+// },{
+//   nombre: "Erik Magnus Lehnsherr",
+//   edad: 49,
+//   mutante: true
+// },{
+//   nombre: "James Logan",
+//   edad: undefined,
+//   mutante: true
+// }];
+
+// // Multiples tipos
+// // cree dos tipos, uno para charles y otro para apocalipsis
+
+// type Charles = {
+//     poder: string,
+//     estatura: number
+// }
+
+// const charles: Charles = {
+//   poder:"psiquico",
+//   estatura: 1.78
+// };
+
+
+// type Apocalipsis = {
+//     lider: boolean,
+//     miembros: string[]
+// }
+
+
+// const apocalipsis: Apocalipsis = {
+//   lider:true,
+//   miembros: ["Magneto","Tormenta","Psylocke","Angel"]
+// }
+
+// console.log(apocalipsis)
+// // Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+// let mystique: (Charles | Apocalipsis);
+
+// mystique = charles;
+// mystique = apocalipsis;
+
+
+//TAREA 2
+// Funciones Básicas
+// function sumar( a: number, b:number ): number{
+//   return a + b;
+// }
+
+// const contar = ( heroes:string[] ):number => {
+//   return heroes.length;
+// }
+// const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+// contar(superHeroes);
+
+// //Parametros por defecto
+// const llamarBatman = ( llamar:boolean = true ): void => {
+//   if( llamar ){
+//     console.log("Batiseñal activada");
+//   }
+// }
+
+// llamarBatman();
+
+// // Rest?
+// const unirheroes = ( ...personas:string[]): string => {
+//   return personas.join(", ");
+// }
+
+// // Tipo funcion
+// const noHaceNada = ( numero:number, texto:string, booleano:boolean, arreglo: string[] ) => {}
+
+// // Crear el tipo de funcion que acepte la funcion "noHaceNada"
+// let noHaceNadaTampoco: (n:number, t:string, b:boolean, a:string[]) => void;
+// noHaceNadaTampoco = noHaceNada
+
+
+
+
+
+
+
+// TAREA #1
+
+// TIPADO NORMAL
+
+//(() => {
+
+//   // Tipos
+//   const batman:string = 'Bruce';
+//   const superman:string = 'Clark';
+
+//   const existe:boolean = false;
+
+//   // Tuplas
+//   const parejaHeroes: [string, string] = [batman,superman];
+//   const villano: [string, number, boolean] = ['Lex Lutor',5,true];
+
+//   // Arreglos
+//   const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+
+//   //Enumeraciones
+//   enum PoderesHeroes {
+//     acuaman = 0,
+//     batman = 1,
+//     flash = 5,
+//     superman = 100
+//   }
+
+//     const fuerzaAcuaman: PoderesHeroes = PoderesHeroes.acuaman;
+//     const fuerzaBatman: PoderesHeroes = PoderesHeroes.batman;
+//     const fuerzaFlash: PoderesHeroes = PoderesHeroes.flash;
+//     const fuerzaSuperman: PoderesHeroes = PoderesHeroes.superman;
+
+//   // Retorno de funciones
+//   function activar_batiseñal():string{
+//     return 'activada';
+//   }
+
+//   function pedir_ayuda():void{
+//     console.log('Auxilio!!!');
+//   }
+
+//   // Aserciones de Tipo
+//   const poder: any = '100';
+//   const largoDelPoder:number = (poder as string).length;
+//   console.log( largoDelPoder );
+
+
+// })()
