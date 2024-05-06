2bb74f71b68e84fa35faeeb720d2009846148614
Fredy Velásquez <macacoloco696@gmail.com>
2024-05-05 22:45:38 -0600

    ts

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,11 @@
-import {Hero} from './classes/Hero'
+import {Hero as SuperHero} from './classes/Hero'
+import { powers } from './data/powers';
 
 
-const ironman = new Hero('Ironman', 1, 55)
+const ironman = new SuperHero('Ironman', 1, 55)
 console.log(ironman)
+
+console.log(powers[0])
 
 
 
