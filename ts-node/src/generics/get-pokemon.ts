f24b7e62afcca2f8050cc1049cbd0f0c26e12c7c
Fredy Velásquez <macacoloco696@gmail.com>
2024-05-07 15:53:40 -0600

    ts

--- ts-node/src/generics/get-pokemon.ts
+++ ts-node/src/generics/get-pokemon.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 
-export const getPokemon = async (pokemonId: number) => {
+export const getPokemon = async (pokemonId: number) : Promise<string> => {
     const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`) //await waits for the promise to resolve
-    console.log(resp)
+    console.log(resp.data.name)
+    return 'hola mundo';
 }
