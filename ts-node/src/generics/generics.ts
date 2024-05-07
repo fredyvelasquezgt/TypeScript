d95bacd639f83a8c2fc73d4064c4413908ef9fef
Fredy Velásquez <macacoloco696@gmail.com>
2024-05-07 12:24:46 -0600

    ts

--- ts-node/src/generics/generics.ts
+++ ts-node/src/generics/generics.ts
@@ -3,3 +3,7 @@
 export const printObject = (argument: any) => {
     console.log(argument)
 }
+
+export function genericFuntion(argument:any) {
+    return argument;
+}
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,2 +1,13 @@
-import { printObject } from './generics/generics';
+import { printObject, genericFuntion } from './generics/generics';
 
+
+// printObject(123)
+
+// printObject({
+//     a:1,
+//     b:2
+// })
+
+// printObject("Hola mundo")
+
+console.log(genericFuntion(123))
