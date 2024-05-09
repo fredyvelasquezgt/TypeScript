3a3043518d2039ed6c26f1816c10a82027bb99cb
Fredy Velásquez <macacoloco696@gmail.com>
2024-05-09 11:34:07 -0600

    ts

--- ts-node/decorators/pokemon.ts
+++ ts-node/decorators/pokemon.ts
@@ -1,5 +1,16 @@
-//class
+//first decorator
 
+function printToConsole(constructor: Function) {
+    console.log(constructor)
+}
+
+//function that returns function
+const printoToConsoleConditional = () => {
+    return () => console.log('hola mundo')
+}
+
+//class
+@printToConsole
 export class Pokemon {
 
     public publicApi: string = 'https://pokeapi.co'
